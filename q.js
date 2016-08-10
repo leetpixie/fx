632921a428c7866059acfce8667daa9fccec3d05
leetpixie <alexgknows@gmail.com>
2016-08-10 20:38:34 +0200

    Update q.js

--- q.js
+++ q.js
@@ -1,1 +1,1 @@
-function using(x){var j = document.createElement("script");j.src = x+".js";document.head.appendChild(j);}
+function using(x){var j = document.createElement("script");j.async=true;j.src = x+".js";document.head.appendChild(j);}
